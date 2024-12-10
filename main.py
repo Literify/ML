1e5c059b3a0ac350ea041120817b7cd4ccdb8acb
Diki Wahyudi <105198020+dikiwahyudi11@users.noreply.github.com>
2024-12-10 08:30:29 +0700

    Update main.py
    update fitur 1 request

--- main.py
+++ main.py
@@ -1,18 +1,45 @@
+# Import Library
 import io
 import pytesseract
 import pandas as pd
+import pickle
 from flask import Flask, request, jsonify
 from PIL import Image
 from feature import fitur1_ML, fitur2_ml
-from fitur1_ML import predict as predict_books
+from fitur1_ML import predict as predict_books_1
 from fitur2_ml import predict_genre_book, recommend_books
 
+# Import Data
 content_df = pd.read_csv("./data/content_df.csv")
+sampled_categories = pd.read_csv('./data/sampled_categories.csv')
+user_ids_df = pd.read_csv('./data/user_ids.csv')
+
+# Import Model
+# Load the cosine similarity matrix from pickle
+cos_sim = pickle.load(open('./model/cosine_similarity.pkl', 'rb'))
+# Convert the cosine similarity matrix to a dense format
+cos_sim_dense = cos_sim.toarray()
+
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def home():
     return jsonify({"message": "Welcome to the Flask API!"})
+
+@app.route("/recommend-book-fitur-1", methods=["POST"])
+def recommend_book_fitur_1():
+    try:
+        # Get the title from the JSON request
+        title = request.files.get("title")
+        if not title:
+            return jsonify({"error": "Title is required"}), 400
+        
+        # Call the predict function
+        result = predict_books_1(title, data=content_df, cos_sim_dense, similarity_weight=0.7, top_n=3)
+        
+        return jsonify(result)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 @app.route("/predict-genre", methods=["POST"])
 def predict_genre():
